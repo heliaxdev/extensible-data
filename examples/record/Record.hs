1104809ebbbb7e40a2b34c00b3edd009dd2db0d3
Andy Morris <hello@andy-morris.xyz>
2020-05-25 16:56:17 +0200

    Remove old bit of example that has been replaced

--- examples/record/Record.hs
+++ examples/record/Record.hs
@@ -12,9 +12,6 @@ R.extendRec "Rec" [] [t|WithString|] $
     R.typeRecX = [("R3", "contents", [t|Int|])]
   }
 
-foo :: Rec -> Rec
-foo r = r { R.beep = False, R.extR1 = "aaaaaa" }
-
 main :: IO ()
 main = print (R1 {beep = True, boop = 4, label1 = "hello"} :: Rec)
   -- type annotation needed until the pat syns get signatures :/
