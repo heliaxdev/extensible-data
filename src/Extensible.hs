edac256598ec6e08c41ec782ff7b9265194167f4
Andy Morris <hello@andy-morris.xyz>
2020-05-29 15:22:25 +0200

    Skip the context for deriving Generic

--- src/Extensible.hs
+++ src/Extensible.hs
@@ -278,6 +278,7 @@ where
 import Language.Haskell.TH as TH
 import Language.Haskell.TH.Syntax
 import Generics.SYB (Data, everywhere, mkT)
+import GHC.Generics (Generic)
 import Control.Monad
 import Data.Functor.Identity
 import Data.Void
@@ -630,10 +631,10 @@ makeInstances :: Config
 makeInstances conf name names ext tvs (SimpleDeriv strat prds) =
   pure $ map make1 prds
  where
-  make1 prd = StandaloneDerivD strat'
-    (map tvPred tvs ++ map allPred names)
-    (prd `AppT` appExtTvs (ConT name) ext tvs)
-   where
+  make1 prd = StandaloneDerivD strat' ctx (prd `AppT` ty) where
+    ty = appExtTvs (ConT name) ext tvs
+    ctx | prd == ConT ''Generic = []
+        | otherwise             = (map tvPred tvs ++ map allPred names)
     tvPred = AppT prd . VarT . tyvarName
     allPred name' = appExtTvs (ConT bname `AppT` prd) ext tvs
       where bname = applyAffix (bundleName conf) name'
